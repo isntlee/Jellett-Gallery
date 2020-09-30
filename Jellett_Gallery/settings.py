6d7a7ea19ef02f858a3c22e28a6a5ea17f24572d
Space-Rx <leedavidmartina@gmail.com>
2020-09-30 13:02:13 +0000

    added initial heroku deployment

--- Jellett_Gallery/settings.py
+++ Jellett_Gallery/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'guh=@*)cwk-d2_$^#ui3pyq=ldk09#f*qh+e$0@s184)e(-#-w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jellett-gallery.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'Jellett_Gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jellett-gallery.wsgi:application 
