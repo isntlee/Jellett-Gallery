6841ab625638356e718d551c2fae36a0ae0d8144
Space-Rx <leedavidmartina@gmail.com>
2020-10-15 16:34:08 +0000

    updated product categories

--- products/models.py
+++ products/models.py
@@ -21,9 +21,12 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category_friendly = models.CharField(max_length=254, null=True, blank=True)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    dimensions = models.CharField(max_length=12, null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True)
     offer = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
