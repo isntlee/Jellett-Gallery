c579bb6c24863f8b8bee299bb9a59343d78fd7bd
Space-Rx <leedavidmartina@gmail.com>
2020-10-20 18:56:11 +0000

    updating bag/contexts.py

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from .models import Order, OrderLineItem
+
+# Create your tests here.
+
+class TestOrder(TestCase):
+    """ Testing the Order model """
+    def test_order(self):
+        string = Order(id = "1", date = "2020-03-20 18:06:34.021189+00:00", first_name = "Test name", last_name = "Test last")
+        self.assertEqual(str(string), "1-2020-03-20 18:06:34.021189+00:00-Test name-Test last")
